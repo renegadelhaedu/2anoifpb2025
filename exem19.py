fde1312f1577bc068d867404be5de4041431ba8f
renegadelhaedu <renegadelha.edu@gmail.com>
2025-08-29 11:03:48 -0300

    aula 2908

--- exem19.py
+++ exem19.py
@@ -18,6 +18,27 @@ animais.append(cachorro1)
 animais.append(gata1)
 animais.append(papag1)
 
-for bicho in animais:
-    print(f'o tutor de {bicho.nome} é {bicho.tutor}')
+nome_pet = input('qual o nome do animal que vc deseja remover da lista')
+for i in range(len(animais)):
+    if nome_pet == animais[i].nome:
+        animais.pop(i)
+        print('Animal removido do cadastro')
+        break
+#------------OU -----------------------------
+indice = -1
+for i in range(len(animais)):
+    if nome_pet == animais[i].nome:
+        indice = i
+
+if indice >= 0:
+    animais.pop(indice)
+    print('animal retirado do cadatro')
+else:
+    print('nao encontramos este animal no cadastro')
+
+
+
+
+
+
 
